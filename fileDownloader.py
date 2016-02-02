c0166f70a4594fb714f8ba2068339fc5638f0912
Ankita Mehta <ankitamehta283@gmail.com>
2016-02-02 09:16:08 +0530

    Update fileDownloader.py

--- fileDownloader.py
+++ fileDownloader.py
@@ -2,13 +2,13 @@ import urllib
 import urllib2
 import time
 
-#Scrapping search engines
+#Downloading URLs
 hosts = ["https://ia800300.us.archive.org/27/items/amusementsinmath16713gut/16713-8.txt","https://ia800300.us.archive.org/27/items/amusementsinmath16713gut/16713.txt","https://ia802605.us.archive.org/18/items/beetonsbookofnee15147gut/15147-8.txt","https://ia802605.us.archive.org/18/items/beetonsbookofnee15147gut/15147.txt","https://ia801405.us.archive.org/18/items/alicesadventures19033gut/19033-8.txt","https://ia801405.us.archive.org/18/items/alicesadventures19033gut/19033.txt"]
 
 #Start timer
 start = time.time()
 
-#grabs urls of hosts and prints first 1024 bytes of page
+#grabs urls of one by one and store it in files
 for host in hosts:
 	#Fetch file name from url
 	file_name = host.split('/')[-1]
